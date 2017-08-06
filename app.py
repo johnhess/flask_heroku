74e57dc4e0f1ff19138d7efca4a2358fba1148e1
John Hess <mgojohn@gmail.com>
2017-08-05 20:55:26 -0400

    basic reflected attack, template for stored

--- app.py
+++ app.py
@@ -1,62 +1,26 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-
-This file creates your application.
-"""
-
 import os
-from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+import flask
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app = flask.Flask(__name__)
 
-
-###
-# Routing for your application.
-###
 
 @app.route('/')
 def home():
-    """Render website's home page."""
-    return render_template('home.html')
+    return flask.render_template('home.html')
 
+@app.route('/reflected')
+def reflected_xss():
+    return 'Hello, {}.'.format(flask.request.args.get('name', 'friend'))
 
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
+@app.route('/stored', methods=['GET', 'POST'])
+def stored_xss():
+    messages = ['foo', 'bar']
+    if flask.request.method == 'POST':
+        messages.append('New')
 
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
+    return flask.render_template('stored.html', messages=messages)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
